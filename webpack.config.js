ea890d90ce92f87cc106fc957b25ead751b03a7d
victor <vikmacon@gmail.com>
2020-05-13 22:31:26 -0400

    configuración basica de un proyecto con webpack y svelte

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+//pasamos a node la ruta del proyecto
+const path = require('path');
+
+//mueve un archivo html manteniendo ciertas configuraciones
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//creamos un modulo con todas las configuraciones de webpack y svelte
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['*', '.mjs', '.js', '.svelte']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.svelte$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'svelte-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        })
+    ]
+}
+
+
